42da2ad226937bf63a706b4e504c7bd3ac6c487b
pguilimaf <pguilimaf@gmail.com>
2021-06-23 16:01:10 -0300

    Botão, Linha e Hierarquia

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,23 @@ void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
       home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text("Pessoas: 0",
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            FlatButton(
+              child: Text("+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white)),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text("-1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white)),
+              onPressed: () {},
+            ),
+          ]),
           Text(
             "Pode entrar!",
             style: TextStyle(
