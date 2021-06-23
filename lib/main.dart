0b61ce8146c2e6ccf0a9a1cf5c57ab3daf4e8b85
pguilimaf <pguilimaf@gmail.com>
2021-06-23 15:36:20 -0300

    Coluna e textos

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Container(
-        color: Colors.white,
+      home: Column(
+        children: <Widget>[
+          Text("Pessoas: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          Text(
+            "Pode entrar!",
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ],
       )));
 }
